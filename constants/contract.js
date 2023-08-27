1d20f072aa5033a27b1255d83172d72808c23a07
Otaiki1 <otaikisadiq2804@gmail.com>
2023-08-27 11:09:08 +0100

    feat: setup context

--- constants/contract.js
+++ constants/contract.js
@@ -1,3 +1,24 @@
-const verbalTokenAddress = "0xbe9a1bD1FA89EbdE4A2381EFd708717c6e011a7C"
-const podcastAddress = "0x3e8FF7427Ea64D38B4885864847FbBbb2408c48A"
-const sessionAddress = "0xc853255C2cD41aF8CAcF5e79480950f250FCeb94"
+import verbalAbi from "./VerbalToken.json";
+import sessionsAbi from "./Sessions.json";
+import podcastAbi from "./PodcastContract.json";
+import rewardsAbi from "./RewardsContract.json";
+
+const VerbalAddress = "0x4100A82F582A09B44A0d2D8A0081B0aF396D2A12";
+const PodcastAddress = "0x190C99181DEdd05894a19e1293831a3F992B75a8";
+const SessionsAddress = "0xe969cC2488238D2408b6E1d92cece37a33E077CC";
+const RewardsAddress = "0x73e5a7A236FF9CB73558433b6Da2880630511629";
+
+const VerbalABI = verbalAbi.abi;
+const SessionsABI = sessionsAbi.abi;
+const PodcastABI = podcastAbi.abi;
+const RewardsABI = rewardsAbi.abi;
+export {
+  VerbalAddress,
+  VerbalABI,
+  PodcastAddress,
+  PodcastABI,
+  SessionsAddress,
+  SessionsABI,
+  RewardsAddress,
+  RewardsABI,
+};
